1169c3da7af9f23785a7415d1db538e8f1f222da
Michael Bien <mbien@fh-landshut.de>
2011-08-10 23:59:45 +0200

    code cleanup, no semantic changes (see actual modifications in the next commit).  - moved variables/initializers/methods into appropriate scope  - generics for collections, use interface when possible  - code formating to common java standards

--- src/java/com/jogamp/common/jvm/JNILibLoaderBase.java
+++ src/java/com/jogamp/common/jvm/JNILibLoaderBase.java
@@ -36,7 +36,6 @@
  * Sun gratefully acknowledges that this software was originally authored
  * and developed by Kenneth Bradley Russell and Christopher John Kline.
  */
-
 package com.jogamp.common.jvm;
 
 import java.lang.reflect.InvocationTargetException;
@@ -44,176 +43,189 @@ import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.AccessControlContext;
 import java.util.HashSet;
+import java.util.Set;
 import jogamp.common.Debug;
 
 public class JNILibLoaderBase {
-  public static final boolean DEBUG = Debug.debug("JNILibLoader");
-  private static final AccessControlContext localACC = AccessController.getContext();
 
-  public interface LoaderAction {
-    /**
-     * Loads the library specified by libname.<br>
-     * The implementation should ignore, if the library has been loaded already.<br>
-     * @param libname the library to load
-     * @param ignoreError if true, errors during loading the library should be ignored 
-     * @return true if library loaded successful
-     */
-    boolean loadLibrary(String libname, boolean ignoreError);
+    public static final boolean DEBUG = Debug.debug("JNILibLoader");
 
-    /**
-     * Loads the library specified by libname.<br>
-     * Optionally preloads the libraries specified by preload.<br>
-     * The implementation should ignore, if any library has been loaded already.<br>
-     * @param libname the library to load
-     * @param preload the libraries to load before loading the main library if not null
-     * @param preloadIgnoreError if true, errors during loading the preload-libraries should be ignored 
-     */
-    void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError);
-  }
-  
-  private static class DefaultAction implements LoaderAction {
-    public boolean loadLibrary(String libname, boolean ignoreError) {
-      boolean res = true;
-      if(!isLoaded(libname)) {
-          try {
-            loadLibraryInternal(libname);
-            addLoaded(libname);
-            if(DEBUG) {
-                System.err.println("JNILibLoaderBase loaded "+libname);
-            }
-          } catch (UnsatisfiedLinkError e) {
-            res = false;
-            if(DEBUG) {
-                e.printStackTrace();
-            }
-            if (!ignoreError && e.getMessage().indexOf("already loaded") < 0) {
-              throw e;
-            }
-          }
-      }
-      return res;
+    private static final Set<String> loaded = new HashSet<String>();
+    private static LoaderAction loaderAction = new DefaultAction();
+
+    public static boolean isLoaded(String libName) {
+        return loaded.contains(libName);
     }
 
-    public void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError) {
-      if(!isLoaded(libname)) {
-          if (null!=preload) {
-            for (int i=0; i<preload.length; i++) {
-              loadLibrary(preload[i], preloadIgnoreError);
-            }
-          }
-          loadLibrary(libname, false);
-      }
-    }
-  }
-
-  private static final HashSet loaded = new HashSet();
-  private static LoaderAction loaderAction = new DefaultAction();
-
-  public static boolean isLoaded(String libName) {
-    return loaded.contains(libName);
-  }
-
-  public static void addLoaded(String libName) {
-    loaded.add(libName);
-    if(DEBUG) {
-        System.err.println("JNILibLoaderBase Loaded Native Library: "+libName);
-    }
-  }
-
-  public static void disableLoading() {
-    setLoadingAction(null);
-  }
-
-  public static void enableLoading() {
-    setLoadingAction(new DefaultAction());
-  }
-  
-  public static synchronized void setLoadingAction(LoaderAction action) {
-    loaderAction = action;
-  }
-
-  protected static synchronized boolean loadLibrary(String libname, boolean ignoreError) {
-    if (loaderAction != null) {
-        return loaderAction.loadLibrary(libname, ignoreError);    
-    }
-    return false;
-  }
-  
-  protected static synchronized void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError) {
-    if (loaderAction != null) {
-        loaderAction.loadLibrary(libname, preload, preloadIgnoreError);    
-    }
-  }
-
-  private static final Class  customLauncherClass;
-  private static final Method customLoadLibraryMethod;
-
-  // FIXME centralize logging
-  static {
-    Class launcherClass = null;
-    Method loadLibraryMethod = null;
-
-    if ( Debug.getBooleanProperty("sun.jnlp.applet.launcher", false, localACC) ) {
-        try {
-            launcherClass = Class.forName("org.jdesktop.applet.util.JNLPAppletLauncher");
-            loadLibraryMethod = launcherClass.getDeclaredMethod("loadLibrary", new Class[] { String.class });
-        } catch (ClassNotFoundException ex) {
-            if(DEBUG) {
-                ex.printStackTrace();
-            }
-        } catch (NoSuchMethodException ex) {
-            if(DEBUG) {
-                ex.printStackTrace();
-            }
-            launcherClass = null;
+    public static void addLoaded(String libName) {
+        loaded.add(libName);
+        if (DEBUG) {
+            System.err.println("JNILibLoaderBase Loaded Native Library: " + libName);
         }
     }
 
-    if(null==launcherClass) {
-        String launcherClassName = Debug.getProperty("jnlp.launcher.class", false, localACC);
-        if(null!=launcherClassName) {
+    public static void disableLoading() {
+        setLoadingAction(null);
+    }
+
+    public static void enableLoading() {
+        setLoadingAction(new DefaultAction());
+    }
+
+    public static synchronized void setLoadingAction(LoaderAction action) {
+        loaderAction = action;
+    }
+
+    protected static synchronized boolean loadLibrary(String libname, boolean ignoreError) {
+        if (loaderAction != null) {
+            return loaderAction.loadLibrary(libname, ignoreError);
+        }
+        return false;
+    }
+
+    protected static synchronized void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError) {
+        if (loaderAction != null) {
+            loaderAction.loadLibrary(libname, preload, preloadIgnoreError);
+        }
+    }
+
+
+    public interface LoaderAction {
+
+        /**
+         * Loads the library specified by libname.
+         * The implementation should ignore, if the library has been loaded already.<br>
+         * @param libname the library to load
+         * @param ignoreError if true, errors during loading the library should be ignored
+         * @return true if library loaded successful
+         */
+        boolean loadLibrary(String libname, boolean ignoreError);
+
+        /**
+         * Loads the library specified by libname.
+         * Optionally preloads the libraries specified by preload.<br>
+         * The implementation should ignore, if any library has been loaded already.<br>
+         * @param libname the library to load
+         * @param preload the libraries to load before loading the main library if not null
+         * @param preloadIgnoreError if true, errors during loading the preload-libraries should be ignored
+         */
+        void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError);
+    }
+
+    private static class DefaultAction implements LoaderAction {
+
+        private static final Method customLoadLibraryMethod;
+        private static final AccessControlContext localACC = AccessController.getContext();
+
+        static {
+
+            Method loadLibraryMethod = null;
+            Class<?> launcherClass = null;
+
+            if (Debug.getBooleanProperty("sun.jnlp.applet.launcher", false, localACC)) {
+                try {
+                    launcherClass = Class.forName("org.jdesktop.applet.util.JNLPAppletLauncher");
+                    loadLibraryMethod = launcherClass.getDeclaredMethod("loadLibrary", new Class[]{String.class});
+                } catch (ClassNotFoundException ex) {
+                    if (DEBUG) {
+                        ex.printStackTrace();
+                    }
+                } catch (NoSuchMethodException ex) {
+                    if (DEBUG) {
+                        ex.printStackTrace();
+                    }
+                    launcherClass = null;
+                }
+            }
+
+            if (null == launcherClass) {
+                String launcherClassName = Debug.getProperty("jnlp.launcher.class", false, localACC);
+                if (null != launcherClassName) {
+                    try {
+                        launcherClass = Class.forName(launcherClassName);
+                        loadLibraryMethod = launcherClass.getDeclaredMethod("loadLibrary", new Class[]{String.class});
+                    } catch (ClassNotFoundException ex) {
+                        if (DEBUG) {
+                            ex.printStackTrace();
+                        }
+                    } catch (NoSuchMethodException ex) {
+                        if (DEBUG) {
+                            ex.printStackTrace();
+                        }
+                        launcherClass = null;
+                    }
+                }
+            }
+
+            customLoadLibraryMethod = loadLibraryMethod;
+        }
+
+        public boolean loadLibrary(String libname, boolean ignoreError) {
+            boolean res = true;
+            if (!isLoaded(libname)) {
+                try {
+                    loadLibraryImpl(libname);
+                    addLoaded(libname);
+                    if (DEBUG) {
+                        System.err.println(getClass() + " loaded " + libname);
+                    }
+                } catch (UnsatisfiedLinkError e) {
+                    res = false;
+                    if (DEBUG) {
+                        e.printStackTrace();
+                    }
+                    if (!ignoreError && e.getMessage().indexOf("already loaded") < 0) {
+                        throw e;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public void loadLibrary(String libname, String[] preload, boolean preloadIgnoreError) {
+            if (!isLoaded(libname)) {
+                if (null != preload) {
+                    for (int i = 0; i < preload.length; i++) {
+                        loadLibrary(preload[i], preloadIgnoreError);
+                    }
+                }
+                loadLibrary(libname, false);
+            }
+        }
+
+        private void loadLibraryImpl(String libraryName) {
+
+            // Note: special-casing JAWT which is built in to the JDK
+            if (null != customLoadLibraryMethod && !libraryName.equals("jawt")) {
+                loadCustom(libraryName);
+            } else{
+                loadFromLibpath(libraryName);
+            }
+        }
+
+        private void loadCustom(String libraryName) {
+
             try {
-                launcherClass = Class.forName(launcherClassName);
-                loadLibraryMethod = launcherClass.getDeclaredMethod("loadLibrary", new Class[] { String.class });
-            } catch (ClassNotFoundException ex) {
-                if(DEBUG) {
-                    ex.printStackTrace();
+                customLoadLibraryMethod.invoke(null, new Object[]{libraryName});
+            } catch (Exception e) {
+                Throwable t = e;
+                if (t instanceof InvocationTargetException) {
+                    t = ((InvocationTargetException) t).getTargetException();
                 }
-            } catch (NoSuchMethodException ex) {
-                if(DEBUG) {
-                    ex.printStackTrace();
+                if (t instanceof Error) {
+                    throw (Error) t;
                 }
-                launcherClass = null;
+                if (t instanceof RuntimeException) {
+                    throw (RuntimeException) t;
+                }
+                // Throw UnsatisfiedLinkError for best compatibility with System.loadLibrary()
+                throw (UnsatisfiedLinkError) new UnsatisfiedLinkError("can not load library " + libraryName).initCause(e);
             }
         }
-    }
 
-    customLauncherClass = launcherClass;
-    customLoadLibraryMethod = loadLibraryMethod;
-  }
-
-  private static void loadLibraryInternal(String libraryName) {
-    // Note: special-casing JAWT which is built in to the JDK
-    if (null!=customLoadLibraryMethod && !libraryName.equals("jawt")) {
-        try {
-          customLoadLibraryMethod.invoke(null, new Object[] { libraryName });
-        } catch (Exception e) {
-          Throwable t = e;
-          if (t instanceof InvocationTargetException) {
-            t = ((InvocationTargetException) t).getTargetException();
-          }
-          if (t instanceof Error) {
-            throw (Error) t;
-          }
-          if (t instanceof RuntimeException) {
-            throw (RuntimeException) t;
-          }
-          // Throw UnsatisfiedLinkError for best compatibility with System.loadLibrary()
-          throw (UnsatisfiedLinkError) new UnsatisfiedLinkError("can not load library "+libraryName).initCause(e);
+        private void loadFromLibpath(String libraryName) {
+            System.loadLibrary(libraryName);
         }
-    } else {
-      // System.err.println("sun.boot.library.path=" + Debug.getProperty("sun.boot.library.path", false));
-      System.loadLibrary(libraryName);
+
     }
-  }
 }
